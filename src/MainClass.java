d4d08104b083e18eee1d2c7704c77e34d383ee42
Kirill <104092448+KirillSolo@users.noreply.github.com>
2022-04-24 20:54:59 +0300

    Initial commit

---
+++ src/MainClass.java
@@ -0,0 +1,22 @@
+public class MainClass
+{
+    private String class_string ="Hello, world";
+    private int class_number = 20;
+
+    public int getLocalNumber ()
+    {
+        return 14;
+    }
+
+    public int getClassNumber()
+    {
+        return class_number;
+    }
+
+    public String getClassString()
+    {
+        return class_string;
+    }
+}
+
+
---
+++ src/MainClassTest.java
@@ -0,0 +1,45 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+public class MainClassTest extends MainClass
+{
+    MainClass Main = new MainClass ();
+
+    @Test
+    public void testGetLocalNumber()
+    {
+        int a = 14;
+        int b = Main.getLocalNumber();
+
+          if (a == b) {
+        System.out.println("The numerical value is 14.");
+          } else {
+              System.out.println("Numeric value is not 14");
+          }
+    }
+
+    @Test
+    public void testGetClassNumber()
+    {
+        int a = Main.getClassNumber();
+        int b = 45;
+
+        if (a > b) {
+            System.out.println("The getClassNumber method returns a value greater than 45.");
+        } else {
+            System.out.println("The getClassNumber method returns a value less than 45.");
+        }
+    }
+
+    @Test
+    public void testGetClassString()
+    {
+        String a = this.getClassString();
+        if (a.contains("Hello") || a.contains("hello"))
+        {
+            System.out.println("There is a substring \"hello\" or \"Hello\" in the getClassString method");
+        } else {
+            Assert.fail("There is not a substring \"hello\" or \"Hello\" in the getClassString method");
+        }
+    }
+}
